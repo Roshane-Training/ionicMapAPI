6090bf0567d3a8fd1f6d6e447fee77281196b43a
Roshane <39389600+Roshane-Johnson@users.noreply.github.com>
2022-08-28 12:26:53 -0500

    updated directions demo

--- src/app/pages/direction/direction.page.ts
+++ src/app/pages/direction/direction.page.ts
@@ -18,6 +18,16 @@ export class DirectionPage implements OnInit, AfterViewInit {
 	@ViewChild('end') end: ElementRef<HTMLInputElement>;
 
 	currentDirection: any;
+	shape = {
+		coords: [1, 1, 1, 20, 18, 20, 18, 1],
+		type: 'poly',
+	};
+	markerA = new google.maps.Marker();
+	markerB = new google.maps.Marker({
+		draggable: true,
+		label: 'D',
+		animation: google.maps.Animation.DROP,
+	});
 
 	constructor(private ngZone: NgZone) {}
 	ngOnInit(): void {}
@@ -27,20 +37,27 @@ export class DirectionPage implements OnInit, AfterViewInit {
 	}
 
 	initMap(): void {
-		const directionsService = new google.maps.DirectionsService();
-		const directionsRenderer = new google.maps.DirectionsRenderer({
-			draggable: true,
-		});
-
 		const map = new google.maps.Map(this.map.nativeElement, {
 			zoom: 15,
 			center: { lng: -76.8019, lat: 17.9962 },
 		});
 
-		directionsRenderer.setMap(map);
+		const directionsService = new google.maps.DirectionsService();
+		const directionsRenderer = new google.maps.DirectionsRenderer({
+			draggable: true,
+			map,
+		});
 
-		const onChangeHandler = () => {
-			this.calculateAndDisplayRoute(directionsService, directionsRenderer);
+		const onChangeHandler = (
+			originQuery: string,
+			destinationQuery: string
+		) => {
+			this.calculateAndDisplayRoute(
+				directionsService,
+				directionsRenderer,
+				originQuery,
+				destinationQuery
+			);
 		};
 
 		const options = {
@@ -59,35 +76,108 @@ export class DirectionPage implements OnInit, AfterViewInit {
 		);
 
 		autocompleteA.addListener('place_changed', () => {
-			(this.end.nativeElement.value !== '' ? onChangeHandler : () => {})();
+			this.ngZone.run(() => {
+				const place = autocompleteA.getPlace();
+
+				if (this.end.nativeElement.value !== '') {
+					onChangeHandler(
+						this.start.nativeElement.value.trim(),
+						this.end.nativeElement.value.trim()
+					);
+				}
+
+				this.markerA.setOptions({
+					position: {
+						lat: place.geometry.location.lat(),
+						lng: place.geometry.location.lng(),
+					},
+					map,
+				});
+
+				// map.panTo(this.markerA.getPosition() as google.maps.LatLng);
+			});
 		});
 
 		autocompleteB.addListener('place_changed', () => {
-			(this.start.nativeElement.value !== '' ? onChangeHandler : () => {})();
+			this.ngZone.run(() => {
+				const place = autocompleteB.getPlace();
+
+				if (this.start.nativeElement.value !== '') {
+					onChangeHandler(
+						this.start.nativeElement.value.trim(),
+						this.end.nativeElement.value.trim()
+					);
+				}
+
+				this.markerB.setOptions({
+					position: {
+						lat: place.geometry.location.lat(),
+						lng: place.geometry.location.lng(),
+					},
+					map,
+					draggable: true,
+				});
+
+				// map.panTo(this.markerB.getPosition() as google.maps.LatLng);
+			});
 		});
 	}
 
 	calculateAndDisplayRoute(
 		directionsService: google.maps.DirectionsService,
-		directionsRenderer: google.maps.DirectionsRenderer
+		directionsRenderer: google.maps.DirectionsRenderer,
+		origin:
+			| string
+			| google.maps.LatLng
+			| google.maps.LatLngLiteral
+			| google.maps.Place,
+		destination:
+			| string
+			| google.maps.LatLng
+			| google.maps.LatLngLiteral
+			| google.maps.Place
 	) {
 		directionsService
 			.route({
-				origin: {
-					query: this.start.nativeElement.value.trim(),
-				},
-				destination: {
-					query: this.end.nativeElement.value.trim(),
-				},
+				origin,
+				destination,
 				travelMode: google.maps.TravelMode.DRIVING,
+				avoidTolls: true,
+				optimizeWaypoints: true,
 				drivingOptions: {
 					departureTime: new Date(),
 				},
 			})
 			.then((response) => {
 				directionsRenderer.setDirections(response);
+				directionsRenderer.setOptions({ suppressMarkers: true });
 				this.currentDirection = directionsRenderer.getDirections();
-				console.log(this.currentDirection);
+
+				this.markerB.addListener('dragend', () => {
+					const latLng = this.markerB.getPosition();
+
+					if (this.start.nativeElement.value !== '') {
+						this.calculateAndDisplayRoute(
+							directionsService,
+							directionsRenderer,
+							this.start.nativeElement.value,
+							latLng
+						);
+					}
+				});
+
+				this.markerA.addListener('dragend', () => {
+					const latLng = this.markerA.getPosition();
+
+					if (this.end.nativeElement.value !== '') {
+						this.calculateAndDisplayRoute(
+							directionsService,
+							directionsRenderer,
+							this.end.nativeElement.value,
+							latLng
+						);
+					}
+				});
 			})
 			.catch((e) => console.log(e));
 	}
